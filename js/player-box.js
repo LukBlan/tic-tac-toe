049e9625c43703e7694281c79ed5c3674311e7c4
Lukblan <luk.blanko@gmail.com>
2023-02-14 19:06:31 -0300

    Player2Box now use newOpponentBox function

--- js/player-box.js
+++ js/player-box.js
@@ -1,6 +1,6 @@
 const playerBox = (function() {
   const player1Box = elementsFactory.newPlayerBox();
-  const player2Box = elementsFactory.newPlayerBox();
+  const player2Box = elementsFactory.newOpponentBox();
 
   function getPlayersBox() {
     return {player1: player1Box, player2: player2Box}
