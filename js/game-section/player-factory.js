14add67953f632e4845b72b3434fbdedfd2f9e5d
Lukblan <luk.blanko@gmail.com>
2023-02-22 19:03:53 -0300

    Refactor playerFactor function

--- js/game-section/player-factory.js
+++ js/game-section/player-factory.js
@@ -1,29 +1,34 @@
 const playerFactory = (function() {
   const playerFunctions = {
-    "Player": function (characterSrc, border, number, playerShadow) {
-       return {
-         characterSrc: characterSrc,
-         characterBorder: border,
-         choice: null,
-         playerNumber: number,
-         shadow: playerShadow,
-         startTurn: function () {
-           pubSub.emit("playerMove", this)
-         },
-         makeMove: function (number) {
-           this.choice = number;
-           pubSub.emit("checkChoice", this);
-           },
-         finishTurn: function() {
-           pubSub.emit("disablePlayerMove", null)
-           pubSub.emit("nextTurn", null)
-         }
-       }
+    "Player": {
+      create: function (characterSrc, border, number, playerShadow) {
+        const currentPlayer = Object.create(this);
+        currentPlayer.characterSrc = characterSrc;
+        currentPlayer.characterBorder = border;
+        currentPlayer.choice = null;
+        currentPlayer.playerNumber = number;
+        currentPlayer.shadow = playerShadow;
+        return currentPlayer;
+      },
+
+      startTurn: function () {
+        pubSub.emit("playerMove", this)
+      },
+
+      makeMove: function (number) {
+        this.choice = number;
+        pubSub.emit("checkChoice", this);
+      },
+
+      finishTurn: function() {
+        pubSub.emit("disablePlayerMove", null)
+        pubSub.emit("nextTurn", null)
+      },
     },
   }
 
   function newPlayer(playerType, characterSrc, border, number, playerShadow) {
-    return playerFunctions[playerType](characterSrc, border, number, playerShadow);
+    return playerFunctions[playerType].create(characterSrc, border, number, playerShadow);
   }
 
   return {newPlayer}
