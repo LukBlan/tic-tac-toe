e75ee693e39d557539981c876ff62b0292f6aedf
Lukblan <luk.blanko@gmail.com>
2023-02-22 19:02:21 -0300

    Add new battle music to game

--- js/music/music-controller.js
+++ js/music/music-controller.js
@@ -3,25 +3,36 @@
   let muteAudio = false;
   const getReady = new Audio('./sounds/get-ready.mp3');
   const characterSelection = new Audio('./sounds/character-selection.mp3');
+  const battleMusic = new Audio('./sounds/battle-music.mp3');
   let currentMusic = null;
 
   pubSub.subscribe("playCharacterSelectionMusic", playCharacterSelection)
   pubSub.subscribe("playGetReadyAudio", playGetReady)
   pubSub.subscribe("toggleMusic", toggleMusic);
+  pubSub.subscribe("playBattleMusic", playBattleMusic);
 
   function toggleMusic() {
     muteAudio = !muteAudio;
     currentMusic.muted = muteAudio;
   }
 
-  function playCharacterSelection() {
-    characterSelection.muted = muteAudio;
-    characterSelection.currentTime = 0;
-    characterSelection.volume = 1;
-    characterSelection.loop = true;
-    currentMusic = characterSelection
-    characterSelection.play();
+  function playBattleMusic() {
+    playMusic(battleMusic)
   }
+
+  function playCharacterSelection() {
+    playMusic(characterSelection);
+  }
+
+  function playMusic(musicTrack) {
+    musicTrack.muted = muteAudio;
+    musicTrack.currentTime = 0;
+    musicTrack.volume = 1;
+    musicTrack.loop = true;
+    currentMusic = musicTrack
+    musicTrack.play();
+  }
+
 
   function playGetReady() {
     characterSelection.pause();
