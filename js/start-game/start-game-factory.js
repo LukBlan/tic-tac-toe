d2aed2acf8198bf7b6448cb8ae501c7ae5d47a4a
Lukblan <luk.blanko@gmail.com>
2023-08-18 21:13:26 -0300

    Change start game button style

--- js/start-game/start-game-factory.js
+++ js/start-game/start-game-factory.js
@@ -1,7 +1,7 @@
 const startGameFactory = (function() {
   function getStartGameScreen() {
     const container = elementsFactory.createElement("div", "start-game-screen", null);
-    const startGameButton = elementsFactory.createElement("button", "start-game-button", "Start Game")
+    const startGameButton = elementsFactory.createElement("button", "start-game-button", "Start")
     container.append(startGameButton)
     return container;
   }
